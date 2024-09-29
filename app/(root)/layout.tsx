ee45f6e70db26932ad5b46a345b9daa2de22ab13
Sub-Par-Golfer <joshjthompson@yahoo.com>
2024-09-29 16:24:43 -0400

    implement 31854

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,20 @@
+'use client'
+
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-const loggedIn = { firstName: 'Josh', lastName: 'Thompson'};
+const router = useRouter();
+const loggedIn = await getLoggedInUser();
+
+if(!loggedIn) router.push('/')
 
   return (
     <main className="flex h-sceen w-full font-inter">
