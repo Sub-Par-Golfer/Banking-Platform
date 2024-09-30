e3ebd76afec0a772a6a75081fc44ffae6a33a1aa
Sub-Par-Golfer <joshjthompson@yahoo.com>
2024-09-30 01:18:37 -0400

    implement 32855

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,14 +3,14 @@ import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
-import { redirect } from "next/navigation";
+import { redirect, useRouter } from "next/navigation";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
+  
 const loggedIn = await getLoggedInUser();
 
 if(!loggedIn) redirect('/sign-in')
